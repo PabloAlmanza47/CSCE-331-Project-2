b011c14048a92ac605a2286d41b29815977bea0e
educatedgambler <educatedgamblers1@gmail.com>
2026-09-24 12:03:40 -0500

    added 2nd validation script, fixed 1st script

--- SQL Scripts/validation.sql
+++ SQL Scripts/validation.sql
@@ -2,10 +2,31 @@
 
 SELECT
     DATE_TRUNC('week', timestamp) AS week,
-    COUNT(*) AS order_count,
+    COUNT(*) AS order_count
 FROM Receipt 
 WHERE timestamp >= CURRENT_DATE - INTERVAL '52 weeks'
 GROUP BY DATE_TRUNC('week', timestamp)
 ORDER BY week;
 
 -- Orders fall within operating hours
+
+SELECT 
+    DATE_TRUNC('hour', timestamp) AS hour,
+    COUNT(*) as order_count,
+    SUM(total_cost) AS total_sales
+FROM Receipt
+GROUP BY EXTRACT(HOUR FROM "timestamp")
+ORDER BY hour;
+
+-- 2 peak days
+
+-- Inventory items for 20 menu items
+
+-- Best of the Worst
+
+--etc
+
+--etc
+
+--etc
+
